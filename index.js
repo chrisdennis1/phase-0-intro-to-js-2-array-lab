57036275521aaa790dbb398472d21f9ed3dab853
chrisdennis12345@gmail.com <chrisdennis@Chriss-iMac.local>
2022-08-09 18:12:59 -0700

    commit tree 4c8d3251e167327a6f655f9d84f8d87e0938a706

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"]  
+
+beforeEach(function () {
+    cats.length = 0;
+
+    cats.push('Milo', 'Otis', 'Garfield');
+  });
+
+  function destructivelyAppendCat(name) {
+    cats.push("Ralph");
+}
+
+function destructivelyPrependCat(name) {
+    cats.unshift("Bob")
+}
+
+function destructivelyRemoveLastCat() {
+    cats.pop();
+}
+
+function destructivelyRemoveFirstCat() {
+cats.shift()
+}
+
+function appendCat(name) {
+ const newCat= [...cats, "Broom"]
+return newCat;
+}
+
+function prependCat(Arnold){
+var newCats= ["Arnold", ...cats]
+return newCats;
+}
+
+function removeLastCat() {
+var gatos= cats.slice(0, cats.length - 1)
+return gatos;
+}
+
+function removeFirstCat() {
+var kitties= cats.slice(1)
+return kitties;
+}
